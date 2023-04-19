375730e4c3f95a26be4ff9dff1ca9e742d2c44c5
Artur <astoriy_06@mail.ru>
2023-04-19 17:43:20 +0600

    movie_detail and movie_list

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.MoviesView.as_view()),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'), #в url будем принимать pk(id) как число
+    # path('<int:pk>/', views.MovieDetailView.as_view()),
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -5,6 +5,19 @@ from django.views.generic.base import View
 # Create your views here.
 from .models import Movie
 
+
+# class MoviesView(View):
+#     def get(self, request):
+#         movies = Movie.objects.all()
+#         return render(request, 'movies/movies.html', {'movie_list': movies})
+
+# class MovieDetailView(View):
+#     def get(self, request, slug):
+#         movie = Movie.objects.get(url=slug) #будем искать фильм по полю url который равен slug
+#         return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+
+
 #отвечает за отображение записей
 class MoviesView(ListView): #наследуемся от класса Django View
     """Список фильмов"""
